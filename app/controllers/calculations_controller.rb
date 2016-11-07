a1818787d226e94426a9722444a41b245dd4575a
Natalia Botia <nbotia@users.noreply.github.com>
2016-11-07 17:19:14 -0600

    My changes

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,36 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_it
+    @to_square=params[:square]
+    render("square_it")
+  end
+
+  def square_root
+    @number=params[:id]
+    render("square_root")
+  end
+
+  def random_min_max
+        @minimo=params[:minimo]
+        @maximo=params[:maximo]
+        render("random_min_max")
+  end
+
+  def payment
+        @irr=params[:interest_rate].to_f
+        @years=params[:years].to_i
+        @principal=params[:principal].to_f
+
+        new_interest_rate = @irr/1200
+        months=@years*12
+        numerator=new_interest_rate*@principal
+        denominator=1-(1+new_interest_rate)**(-months)
+
+        @monthly_payment = numerator/denominator
+
+        render("payment")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get('/square/:square', { :controller => 'calculations', :action => 'square_it' })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+  get("/random/:minimo/:maximo", { :controller => "calculations", :action => "random_min_max" })
+  get("/payment/:interest_rate/:years/:principal", { :controller => "calculations", :action => "payment" })
+
 end
